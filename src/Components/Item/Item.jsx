0b331ef5f92407f213db0819cbafdec64524ecb0
MatiasSotoMaior <matysotomaiorfer@gmail.com>
2023-03-04 18:47:12 -0300

    details

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -4,16 +4,19 @@ import { Link } from 'react-router-dom'
 
 const Item = ({ info }) => {
     return (
-        <Card className='col-6' style={{ width: '18rem', margin:'1em' }}>
+        <Card className='col-6' style={{ width: '18rem', margin: '1em' }}>
             <Card.Img variant="top" src={info.Img} />
             <Card.Body className='text-center'>
-                <Card.Title>{info.Name}</Card.Title>
-                <Card.Text>
-                    {info.Tipo}
-                </Card.Text>
-                <Link to={`/detail/${info.id}`}>
-                    <Button variant="primary">info</Button>
-                </Link>
+                <div>
+                    <Card.Title>{info.Name}</Card.Title>
+                    <Card.Text>{info.Tipo}</Card.Text>
+                </div>
+                <div className='d-flex justify-content-center mt-3'>
+                    <p className='me-3 fs-4'>${info.Precio}</p>
+                    <Link to={`/detail/${info.id}`}>
+                        <Button variant="primary">info</Button>
+                    </Link>
+                </div>
             </Card.Body>
         </Card>
     )
